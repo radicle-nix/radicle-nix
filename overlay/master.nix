a4e2f036bb06c68d74063417bfcf728d40f703a5
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2024-05-22 00:07:56 +0200

    radicle-node-master: unstable-2024-05-22 -> 749e8239852aa4591277d946e585607dc4f464aa

--- overlay/master.nix
+++ overlay/master.nix
@@ -5,14 +5,14 @@ lib.overlayRustPackage rec {
   new = "${old}-master";
   override = args: {
     name = new;
-    version = "064ece32ac0a0bd0efe4f459dcb0462bafc236e6";
+    version = "749e8239852aa4591277d946e585607dc4f464aa";
 
     src = final.fetchgit {
       inherit (args.src) url;
-      rev = "064ece32ac0a0bd0efe4f459dcb0462bafc236e6";
-      hash = "sha256-IDLai0nnjTL54z3J7XmFKCndRiMLrhdhPF2YKGPgDlU=";
+      rev = "749e8239852aa4591277d946e585607dc4f464aa";
+      hash = "sha256-GEzxCuKRnXLItv0vnjgHwSr+tdagnmlLn1K7S1npGZs=";
     };
 
-    cargoHash = "sha256-NJv2MDMH6K/6xnxYcTFUkXUsfuPaxwE3XupkbjPM9MA=";
+    cargoHash = "sha256-2Ab4JlTS2WOKIHmTG7Upj2Fl63rXW1+aNPcpEstseao=";
   };
 }
