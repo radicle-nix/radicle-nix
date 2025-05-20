bb17d7c8179077e03b08fb9cbfbb02723046ffd7
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2025-05-20 10:08:47 +0200

    overlay/master: Update

--- overlay/master.nix
+++ overlay/master.nix
@@ -1,5 +1,5 @@
 {lib}: final: prev: let
-  head = "f30760d6bb86d2978a5ed4df8ee45b9aa97778b4";
+  head = "41f9048d96596473600e6c77f041d29cd3de1d6f";
 in
   lib.overlayRustPackage rec {
     inherit final prev;
@@ -12,7 +12,7 @@ in
       src = final.fetchgit {
         inherit (args.src) url;
         rev = head;
-        hash = "sha256-djrxOBeP2Nkk06Qsf46H8nikxALkntIG39AlVqs0G7E=";
+        hash = "sha256-jhePgLG+vjDp56bIbt7rUNN9lyhqpnvtSmx176gDZrc=";
       };
 
       patches = [];
@@ -25,6 +25,27 @@ in
   // (lib.overlayRustPackage rec {
     inherit final prev;
     old = "radicle-node";
+    new = "${old}-1_2-pre";
+    override = args: {
+      name = new;
+      version = head;
+
+      src = final.fetchgit {
+        inherit (args.src) url;
+        rev = "refs/namespaces/z6MksFqXN3Yhqk8pTJdUGLwATkRfQvwZXPqR2qMEhbS9wzpT/refs/tags/v1.2.0-pre";
+        hash = "sha256-jhePgLG+vjDp56bIbt7rUNN9lyhqpnvtSmx176gDZrc=";
+      };
+
+      patches = [];
+
+      cargoHash = "sha256-SIoHy21SPLAMFk9XwbfKCr9fgV/YzA2j4zT4zvBCbb0=";
+      doCheck = false;
+      passthru = {};
+    };
+  })
+  // (lib.overlayRustPackage rec {
+    inherit final prev;
+    old = "radicle-node";
     new = "${old}-cref";
     override = args: {
       name = new;
@@ -33,7 +54,7 @@ in
       src = final.fetchgit {
         inherit (args.src) url;
         rev = head;
-        hash = "sha256-djrxOBeP2Nkk06Qsf46H8nikxALkntIG39AlVqs0G7E=";
+        hash = "sha256-jhePgLG+vjDp56bIbt7rUNN9lyhqpnvtSmx176gDZrc=";
       };
 
       patches = [];
@@ -45,11 +66,11 @@ in
             rid = "rad:z3gqcJUoA1n9HaHKufZs5FCSGazv5";
             nid = "z6MkireRatUThvd3qzfKht1S44wpm4FEWSSa4PRMTSQZ3voM";
             oid = "c54883e5ffb1f8a99f432e3ac79c0b728cd0dab3";
-            hash = "sha256-T2B06hyFrw6R2D9UydsYCBNrJzB7Yl9CGkoecq96pbo=";
+            hash = "sha256-7UHgddec0q3xTGEhoEd83XT7rhTg0pp0T+HkY6k5AQ4=";
           })
         ];
 
-      cargoHash = "sha256-oNkYjz/A5WrFNHSsudPn5yNSVnDFnuYrPMalhoqAm20=";
+      cargoHash = "sha256-CM4VDsAuLzqQvL9vFJjv2inYKc+LnlV4sEnD6iFYqSo=";
       doCheck = false;
       passthru = {};
     };
